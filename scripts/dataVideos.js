f86f6990c4d177f51f203a5f9fdd962b39bfc0b7
mario-cartagena <jhonmariocartagena@gmail.com>
2023-03-17 10:25:03 -0500

    Add: finalized dataVideos.js and add others properties.

--- scripts/dataVideos.js
+++ scripts/dataVideos.js
@@ -1,96 +1,197 @@
 export const videos = [
-    {
-      id: 1,
-      name: "Curso de CSS desde CERO (Completo)",
-      video: "https://www.youtube.com/watch?v=OWKXEJN67FE",
-      autorImage:
-        "https://yt3.ggpht.com/jHpWdf9qWNdrCjxfQxCxR0aiYY9Y1IO7s6Jwlk-OgKxS8nzjEMffakE6mdOdp2u22R3130U0nQ=s68-c-k-c0x00ffffff-no-rj",
-      autorName: "Soy Dalto",
-      image:
-        "https://i.ytimg.com/vi/OWKXEJN67FE/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDmFbGpt6l467mb_phQlBX4rWDYWA",
-      viewers: "330 vistas",
-      date: "10 ago. 2021"
-    },
-    {
-      id: 2,
-      name: "La Vida de un PROGRAMADOR WEB (Cómo ser un Desarrollador Web, Con qué lenguaje empezar)",
-      video: "https://www.youtube.com/watch?v=rnmbxqS1nRo",
-      autorImage:
-        "https://yt3.ggpht.com/ytc/AL5GRJWoQZZU9vYplLEjO4_39R4jyXRmk7ccKqehGCeUfw=s68-c-k-c0x00ffffff-no-rj",
-      autorName: "Absolute",
-      image:
-        "https://i.ytimg.com/vi/rnmbxqS1nRo/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDNINha6OpGNW6hS92REsLWHnpw8Q",
-      viewers: "330 vistas",
-      date: "10 ago. 2021"
-    },
-    {
-      id: 3,
-      name: "Ruta para ser programador web en el 2023 💻",
-      video: "https://www.youtube.com/watch?v=6tfgKv93C_Q",
-      autorImage:
-        "https://yt3.ggpht.com/QAKnwF21V1gXr1I8O2ZQNBDdS6OvF7JLAEZWLprKZp9Vxe4_6NDZn32yhOSPmY5c-S7nQBN2=s68-c-k-c0x00ffffff-no-rj",
-      autorName: "Programador X",
-      image:
-        "https://i.ytimg.com/vi/6tfgKv93C_Q/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC3JtjyVaUu6XUoOkCeIG8wRnVWbw",
-    },
-    {
-        id: 4,
-        name: "¿Cuánto gana un PROGRAMADOR JUNIOR en ARGENTINA?💸Tips para comenzar 💻",
-        video: "https://www.youtube.com/watch?v=tGHUBzMO9Hw",
-        autorImage:
-          "https://yt3.ggpht.com/tt_wBPjaNf-cttOrEtlyGcO3UJwUCPwQfrqDlNRX7VPNTH-C-oX7XMPFba80eFFov_NmVq87=s68-c-k-c0x00ffffff-no-rj",
-        autorName: "Estudiar con Manu",
-        image:
-          "https://i.ytimg.com/vi/tGHUBzMO9Hw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAdiVi9E6tTOVTSFDXahW2TUc2EQQ",
-    },
-    {
-        id: 5,
-        name: "8 proyectos en JavaScript que debes hacer para tu PORTAFOLIO",
-        video: "https://www.youtube.com/watch?v=CruepI-pEVU",
-        autorImage:
-          "https://yt3.ggpht.com/rwU607PYF9jK9QL2I85SdfCLVZJGGsxWukuF_LxD0PepnqEIrFVg3W85FOVPDmWdMN1SxyJ7Xi8=s68-c-k-c0x00ffffff-no-rj",
-        autorName: "Platzi",
-        image:
-          "https://i.ytimg.com/vi/CruepI-pEVU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCimTYleCSIHHZKi6PCOyYT7uMz1Q",
-    },
-    {
-        id: 6,
-        name: "Crea tu PORTAFOLIO y consigue trabajo como PROGRAMADOR",
-        video: "https://www.youtube.com/watch?v=oYBC7r5oSzk",
-        autorImage:
-          "https://yt3.ggpht.com/IAnViiWaQn3lcb69mYu88mwZrciSCzlucaWP0SwvN11ti7VbD6ODdRjS2nLV04ui56mdrwo9=s68-c-k-c0x00ffffff-no-rj",
-        autorName: "MoureDev TV",
-        image:
-          "https://i.ytimg.com/vi/oYBC7r5oSzk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD_OoPK7BAs6E11DQixsxI_ATC9fw",
-    },
-    {
-        id: 7,
-        name: "Martin Garrix - Forbidden Voices (Official Music Video)",
-        video: "https://www.youtube.com/watch?v=Zv1QV6lrc_Y",
-        autorImage:
-          "https://yt3.ggpht.com/in7ji16oeon_ypiAMeH-ZdTWSaIC54yZqMsFg0X3uhTBk4MDCuIkRf6TIoQQCqjl20DSdakhSQ=s88-c-k-c0x00ffffff-no-rj",
-        autorName: "Martin Garrix",
-        image:
-          "https://i.ytimg.com/vi/Zv1QV6lrc_Y/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAW6WIMacwLydVFlnVSbbPycUnyFQ",
-    },
-    {
-        id: 8,
-        name: "Avicii - The Nights",
-        video: "https://www.youtube.com/watch?v=UtF6Jej8yb4",
-        autorImage:
-          "https://yt3.ggpht.com/ytc/AL5GRJVBe7w6rN1a8k0AXrL78dbFcylII5iCKft0GXsb4Q=s88-c-k-c0x00ffffff-no-rj",
-        autorName: "Avicii",
-        image:
-          "https://i.ytimg.com/vi/UtF6Jej8yb4/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBGWowbxmMvHnmPJSuRdoIV95sT7w",
-    },
-    // {
-    //     id: 9,
-    //     name: "Chillstep Music para programación / Cyber / Codificación",
-    //     video: "https://www.youtube.com/watch?v=M5QY2_8704o",
-    //     autorImage:
-    //       "https://yt3.ggpht.com/9k0N3CXvGaQj44oSDb-r63amGX2w-orEyp409sTBp0tK4WEuZTJcdhO5XzmWaschG1aCcVjWnA=s68-c-k-c0x00ffffff-no-rj",
-    //     autorName: "Chill Music Lab",
-    //     image:
-    //       "https://i.ytimg.com/vi/M5QY2_8704o/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCCEDQCh5uETjQQedkAdnWkI0_Rbg",
-    // },     
-  ];
+  {
+    id: 1,
+    name: "Curso de CSS desde CERO (Completo)",
+    video: "https://www.youtube.com/watch?v=OWKXEJN67FE",
+    autorImage:
+      "https://yt3.ggpht.com/jHpWdf9qWNdrCjxfQxCxR0aiYY9Y1IO7s6Jwlk-OgKxS8nzjEMffakE6mdOdp2u22R3130U0nQ=s68-c-k-c0x00ffffff-no-rj",
+    autorName: "Soy Dalto",
+    image:
+      "https://i.ytimg.com/vi/OWKXEJN67FE/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDmFbGpt6l467mb_phQlBX4rWDYWA",
+    viewers: "3.196.416 vistas",
+    date: "16 abr. 2021",
+    category: "Programación"
+  },
+  {
+    id: 2,
+    name: "La Vida de un PROGRAMADOR WEB (Cómo ser un Desarrollador Web, Con qué lenguaje empezar)",
+    video: "https://www.youtube.com/watch?v=rnmbxqS1nRo",
+    autorImage:
+      "https://yt3.ggpht.com/ytc/AL5GRJWoQZZU9vYplLEjO4_39R4jyXRmk7ccKqehGCeUfw=s68-c-k-c0x00ffffff-no-rj",
+    autorName: "Absolute",
+    image:
+      "https://i.ytimg.com/vi/rnmbxqS1nRo/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDNINha6OpGNW6hS92REsLWHnpw8Q",
+    viewers: "100.643 vistas",
+    date: "27 mar. 2021",
+    category: "Programación"
+  },
+  {
+    id: 3,
+    name: "Llaneros vs. Jaguares (goles) | Copa BetPlay Dimayor 2023 | Fase 2 - Ida",
+    video: "https://www.youtube.com/watch?v=2N4t89AWrBM",
+    autorImage:
+      "https://yt3.ggpht.com/JaUqZ8sdK9xwdg7Q0PuaawC_24JwZmiF_u3CNMazbLY88bpyma9AII2bkxRAAoKRVFLIliaJH80=s48-c-k-c0x00ffffff-no-rj",
+    autorName: "Win Sports",
+    image:
+      "https://i.ytimg.com/vi/2N4t89AWrBM/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB-hnOP_aGzv79ZgPSw6lBxlunbBw",
+    viewers: "8.754 vistas",
+    date: "hace 1 día",
+    category: "Deportes"
+  },
+  {
+    
+    id: 4,
+    name: "Ruta para ser programador web en el 2023 💻",
+    video: "https://www.youtube.com/watch?v=6tfgKv93C_Q",
+    autorImage:
+      "https://yt3.ggpht.com/QAKnwF21V1gXr1I8O2ZQNBDdS6OvF7JLAEZWLprKZp9Vxe4_6NDZn32yhOSPmY5c-S7nQBN2=s68-c-k-c0x00ffffff-no-rj",
+    autorName: "Programador X",
+    image:
+      "https://i.ytimg.com/vi/6tfgKv93C_Q/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC3JtjyVaUu6XUoOkCeIG8wRnVWbw",
+    viewers: "118.814 vistas",
+    date: "hace 3 meses",
+    category: "Programación"
+  },
+  {
+    id: 5,
+    name: "8 proyectos en JavaScript que debes hacer para tu PORTAFOLIO",
+    video: "https://www.youtube.com/watch?v=CruepI-pEVU&t=7s",
+    autorImage:
+      "https://yt3.ggpht.com/rwU607PYF9jK9QL2I85SdfCLVZJGGsxWukuF_LxD0PepnqEIrFVg3W85FOVPDmWdMN1SxyJ7Xi8=s68-c-k-c0x00ffffff-no-rj",
+    autorName: "Platzi",
+    image:
+      "https://i.ytimg.com/vi/CruepI-pEVU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCimTYleCSIHHZKi6PCOyYT7uMz1Q",
+    viewers: "28.179 visitas",
+    date: "hace 7 meses",
+    category: "Programación"
+  },
+  {
+    id: 6,
+    name: "Avicii - The Nights",
+    video: "https://www.youtube.com/watch?v=UtF6Jej8yb4",
+    autorImage:
+      "https://yt3.ggpht.com/ytc/AL5GRJVBe7w6rN1a8k0AXrL78dbFcylII5iCKft0GXsb4Q=s88-c-k-c0x00ffffff-no-rj",
+    autorName: "Avicii",
+    image:
+      "https://i.ytimg.com/vi/UtF6Jej8yb4/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBGWowbxmMvHnmPJSuRdoIV95sT7w",
+    viewers: "901.777.658 visitas",
+    date: "15 dic 2014 ",
+    category: "Música"
+  },
+  {
+    id: 7,
+    name: "Crea tu PORTAFOLIO y consigue trabajo como PROGRAMADOR",
+    video: "https://www.youtube.com/watch?v=oYBC7r5oSzk&t=555s",
+    autorImage:
+      "https://yt3.ggpht.com/IAnViiWaQn3lcb69mYu88mwZrciSCzlucaWP0SwvN11ti7VbD6ODdRjS2nLV04ui56mdrwo9=s68-c-k-c0x00ffffff-no-rj",
+    autorName: "MoureDev TV",
+    image:
+      "https://i.ytimg.com/vi/oYBC7r5oSzk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD_OoPK7BAs6E11DQixsxI_ATC9fw",
+    viewers: "54.639 visitas",
+    date: "hace 2 meses",
+    category: "Programación"  
+  },
+  {
+    id: 8,
+    name: "blink-182 - I Miss You (Official Video)",
+    video: "https://www.youtube.com/watch?v=s1tAYmMjLdY",
+    autorImage:
+      "https://yt3.ggpht.com/_CSj3Z3SGJJPGZU3B7hPJJ8L0ncdNAdP1ZMLoKPkeDwJ2wJ27uH8sEV_qyYTcq-bDv2bpYHhjfI=s48-c-k-c0x00ffffff-no-nd-rj",
+    autorName: "blink-182",
+    image:
+      "https://i.ytimg.com/vi/s1tAYmMjLdY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA64jM8MnNPlH_Xx7SPKo73vxVpqg",
+    viewers: "356.038.299 visitas",
+    date: "17 jun. 2009",
+    category: "Música" 
+  },
+  {
+    id: 9,
+    name: "Galantis - Runaway (U & I) (Official Video)",
+    video: "https://www.youtube.com/watch?v=5XR7naZ_zZA",
+    autorImage:
+      "https://yt3.ggpht.com/gRPr3pEjSXg2Hd0vIyMrHxDLhZ4z0ZZMSuV78uPCXNfdZ3DEA534CS5miTXGZgiyrEInecOtgw=s88-c-k-c0x00ffffff-no-rj",
+    autorName: "Galantis",
+    image:
+      "https://i.ytimg.com/vi/5XR7naZ_zZA/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBRqxg_3yZFR0Dd7JEBUJQ13hzwjQ",
+    viewers: "407.037.931 visitas",
+    date: "19 ene. 2015",
+    category: "Música" 
+  },
+  {
+    id: 10,
+    name: "¡GOLAZO DE MITAD DE CANCHA Y LOS GUNNERS AFUERA DE EUROPA! | Arsenal 1 (3)-(5) Sporting CP | RESUMEN",
+    video: "https://www.youtube.com/watch?v=cxFwytqZNOU",
+    autorImage: "https://yt3.ggpht.com/3ondwJtqgg_1YlfjFd_KJMQ-Q4bz764TiYlsZc0kKwsU5FQghNUwalM0jnnPo7WysTCOWkQQIg=s48-c-k-c0x00ffffff-no-rj",
+    autorName: "ESPN Fans",
+    image: "https://i.ytimg.com/vi/cxFwytqZNOU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCv-i2f1vore1TJnfi1-_ajRlUgXw",
+    viewers: "484,173 vistas",
+    date: "hace 1 día",
+    category: "Deportes"
+  },
+  {
+    id: 11,
+    name: "Tu Con El",
+    video: "https://www.youtube.com/watch?v=OesXvuWhDnw",
+    autorImage: "https://yt3.ggpht.com/ytc/AL5GRJXayaQKgkg8ugUYHDi9BJ9P0yty-lOokN4x5KCH=s88-c-k-c0x00ffffff-no-rj",
+    autorName: "Frankie Ruiz",
+    image: "https://i.ytimg.com/vi/OesXvuWhDnw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBkQAeURlinXZlOegfE9tz3BDDyWA",
+    viewers: "104.576.187 vistas",
+    date: "27 jul. 2021",
+    category: "Música"
+  },
+  {
+    id: 12,
+    name: "Aprende Git y GitHub - Curso desde Cero",
+    video: "https://www.youtube.com/watch?v=mBYSUUnMt9M",
+    autorImage: "https://yt3.ggpht.com/Q_KD8PJy-rOefmOPEPoGXl9jXlSRToONNygA6hZgkepXJTrgGDnHQr_UguNRvPHNqK7GcBoVmw=s68-c-k-c0x00ffffff-no-rj",
+    autorName: "freeCodeCamp Español",
+    image: "https://i.ytimg.com/vi/mBYSUUnMt9M/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBUcakuU0fVP8ka2aN3_zroSMCPOQ",
+    viewers: "48.258 vistas",
+    date: "hace 1 mes",
+    category: "Programación"
+  },
+  {
+    id: 13,
+    name: "MILLONARIOS vs. ATLÉTICO-MG | HIGHLIGHTS | CONMEBOL LIBERTADORES 2023",
+    video: "https://www.youtube.com/watch?v=nxgowRH3t5U",
+    autorImage: "https://yt3.ggpht.com/4OpSFGdKV9eHfn7wVC7rEU75zQNnyozNO0eBOP91oHDefT2gipCn_o0ZZ8qOvCloT5LC2vIqzA=s68-c-k-c0x00ffffff-no-rj",
+    autorName: "CONMEBOL Libertadores",
+    image: "https://i.ytimg.com/vi/nxgowRH3t5U/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBX1fGiY9sBaVwzqN7yPl3GlexuVg",
+    viewers: "82.504 vistas",
+    date: "8 mar. 2023 ",
+    category: "Deportes"
+  },
+  {
+    id: 14,
+    name: "Principios SOLID en React.js (Buenas prácticas) con ejemplos prácticos",
+    video: "https://www.youtube.com/watch?v=jKdt-BnTTR0",
+    autorImage: "https://yt3.ggpht.com/ytc/AL5GRJXkOPy-CnRZKux_psgUwL_BqW0JST-GemEHjBPc=s68-c-k-c0x00ffffff-no-rj",
+    autorName: "midulive",
+    image: "https://i.ytimg.com/vi/jKdt-BnTTR0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBkAAu4gXRN7sOB-5fi9tSK2TuP4w",
+    viewers: "67.885 vistas",
+    date: "29 jul. 2022",
+    category: "Programación"
+  },
+  {
+    id: 15,
+    name: "GPT-4 y ¿el fin de los programadores? (Otra vez)",
+    video: "https://www.youtube.com/watch?v=uYTTKmG8e-4",
+    autorImage: "https://yt3.ggpht.com/ytc/AL5GRJV-I1VE-q87CKx_5E0v-t-3CfgyRFXL23DtZSY1qw=s68-c-k-c0x00ffffff-no-rj",
+    autorName: "Fazt",
+    image: "https://i.ytimg.com/vi/uYTTKmG8e-4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAFtU7a35jEopxt7cYcJ1gI0adNhQ",
+    viewers: "5.852 vistas",
+    date: "hace 3 horas",
+    category: "Programación"
+  },
+  {
+    id: 16,
+    name: "Every Breath You Take",
+    video: "https://www.youtube.com/watch?v=6cucosmPj-A",
+    autorImage: "https://yt3.ggpht.com/ytc/AL5GRJXN8uOdb00kpv6dVYdgJrRjs3kkPnU9cdAEWn6X=s88-c-k-c0x00ffffff-no-rj",
+    autorName: "The Police",
+    image: "https://i.ytimg.com/vi/6cucosmPj-A/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAhgGC4bFDy3o-77AsGRo1KkWpBWg",
+    viewers: "17.166.263 vistas",
+    date: "31 jul. 2018",
+    categoy: "Música"
+  },
+];
